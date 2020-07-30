310d0ae51ba193a2ba25ed220ed9dcfb77797463
Andre Machado <csantos.machado@gmail.com>
2020-07-30 17:53:01 -0300

    Update models.py

--- gustavo/models.py
+++ gustavo/models.py
@@ -4,8 +4,19 @@ from django.db import models
 
 class Gordinho(models.Model):
     "Generated Model"
+    
+    MAX_LIMIT = 5
+    DATA_PROVIDERS = [
+        'http://free.ipwhois.io/json/',
+        'http://ip-api.com/json/',
+    ]
+    
+    class Types(models.TextChoices):
+        OWNER = 'OW', 'Owner'
+        WORKFORCE = 'WF', 'Workforce'
+    
     idade = models.CharField(max_length=256,)
-    nome = models.CharField(max_length=256,)
+    nome = models.CharField(max_length=256, choices=Types.choices, default=Types.OWNER)
 
 
 # Create your models here.
